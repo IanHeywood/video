4fa761e66ec294f4204a05430590847a5e19b190
IanHeywood <IanHeywood@users.noreply.github.com>
2015-01-22 10:23:43 +1100

    Update 02_pre_process.py

--- 02_pre_process.py
+++ 02_pre_process.py
@@ -6,5 +6,5 @@ msName = glob.glob('sb*VIDEO*.ms')[0]
 os.system('split-ms-spw.py '+msName)
 spwlist = glob.glob('*spw*.ms')
 for myms in spwlist:
-	rflagMS(myms,4.0,4.0,'DATA')
+	rflagMS(myms,5.0,5.0,'DATA')
 
