147301c85b769d640190e940b488da5574b3bc30
IanHeywood <ian.heywood@gmail.com>
2015-01-21 14:43:07 +1100

    Modified 04_make_per_spw_LSM.py

--- 04_make_per_spw_LSM.py
+++ 04_make_per_spw_LSM.py
@@ -1,24 +1,19 @@
 from vidscp import *
 
-msList = []
-
-for i in range(15,16):
-	msList += glob.glob('*spw'+str(i)+'.ms')
-
-for myms in msList:
-	spw = myms.split('_')[-1].replace('.ms','')
+for i in range(0,1):
+	spw = 'spw'+str(i)
 	imageList = glob.glob('*'+spw+'.ms*.fits')
-#	pbList = glob.glob('*'+spw+'.*.pb.fits')
-#	for pb in pbList:
-#		imageList.remove(pb)
 	if len(imageList) != 0:
 		name_list = []
 		for img in imageList:
-			name_list.append(img.replace('.fits',''))
-		info('Base Measurement Set '+myms)
+			xx = img.split('.')
+			if 'pb' in xx or 'model' in xx:
+				info('Ignoring '+img)
+			else:
+				name_list.append(img.replace('.fits',''))
 		info('Considering the following files:')
 		for item in name_list:
-			info(spw+' '+item)			
+			info('    '+spw+' '+item)			
 		lsm_list = makeSkyModels(name_list)
 		#lsm_list = remakeSkyModels(name_list)
 		master_lsm = mergeLSMs(lsm_list)
